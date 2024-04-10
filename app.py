afc81002fd3bd7238a0d83df0485534dc2a83d8f
Sam Chaudry <sam.chaudry@hotmail.co.uk>
2024-04-10 15:06:38 +0100

    db setup and adhoc changes

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@hostname/database_name' #Replace with your details
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
