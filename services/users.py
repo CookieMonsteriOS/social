bf8047e6359eb6ffb9e25595d7704a74bd6c8aec
Sam Chaudry <sam.chaudry@hotmail.co.uk>
2024-04-10 18:20:06 +0100

    try catch add

--- services/users.py
+++ services/users.py
@@ -2,8 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from database.models import Users
 from database.db import database
 
-def add_user_database(name, email):
-    new_user = Users(name=name, email=email)
-    database.session.add(new_user)
-    database.session.commit()
-    return new_user.id
+def add_user_to_database(name: str, email: str) -> int:
+    try:
+        new_user = Users(name=name, email=email)
+        database.session.add(new_user)
+        database.session.commit()
+        return new_user.id
+    except Exception as e:
+        database.session.rollback() 
+        raise e 
